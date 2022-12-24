29086c6825de8dd225516390b3700000b3ed893c
miriammr <miriam.molina.8323@gmail.com>
2022-12-24 10:09:21 -0500

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    FooterComponent,
+    HomeComponent,
+    CurriculumComponent,
+    ContactComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
